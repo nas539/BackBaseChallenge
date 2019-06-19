3b040eb7d7b7f0396a778dc3c9368cd1d8d79267
Nicholas <nicholasallensharp539@outlook.com>
2019-06-19 13:34:24 -0400

    Committing SwiftLintified Code

--- BackBaseChallenge/AppDelegate.swift
+++ BackBaseChallenge/AppDelegate.swift
@@ -8,39 +8,56 @@
 
 import UIKit
 
-@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+  
   var window: UIWindow?
-
-
+  
+  
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    // Override point for customization after application launch.
+    let city = TestData.getSingleResponse()
+    let cities = TestData.getAllCities()
+    let filteredCities = TestData.filterForString(cities: cities, searchString: "UA")
+    let toyCities = TestData.filterForString(cities: cities, searchString: "toy")
+    let bismCities = TestData.filterForString(cities: cities, searchString: "bism")
     return true
   }
-
-  func applicationWillResignActive(_ application: UIApplication) {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-  }
-
-  func applicationDidEnterBackground(_ application: UIApplication) {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-  }
-
-  func applicationWillEnterForeground(_ application: UIApplication) {
-    // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-  }
-
-  func applicationDidBecomeActive(_ application: UIApplication) {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-  }
-
-  func applicationWillTerminate(_ application: UIApplication) {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-  }
-
-
 }
 
+class TestData {
+  
+  static func getSingleResponse() -> Cities {
+    let fileURL = Bundle.main.url(forResource: "SingleResponse", withExtension: "json")!
+    let data = try! Data.init(contentsOf: fileURL)
+    return try! JSONDecoder().decode(Cities.self, from: data)
+  }
+  static func getAllCities() -> [Cities] {
+    let fileURL = Bundle.main.url(forResource: "cities", withExtension: "json")!
+    let data = try! Data(contentsOf: fileURL)
+    var cities = [Cities]()
+    //        DispatchQueue.global().async {
+    cities = try! JSONDecoder().decode([Cities].self, from: data)
+    //        }
+    return cities
+  }
+  
+  static func filterForString(cities: [Cities], searchString: String) -> [Cities] {
+    let startTime = CFAbsoluteTimeGetCurrent()
+    let results = cities.filter { (city) -> Bool in
+      if city.city.lowercased().starts(with: searchString.lowercased()) {
+        return true
+      } else {
+        return false
+      }
+    }
+    let timeElapsed = CFAbsoluteTimeGetCurrent() - startTime
+    print("Time elapsed for filtering: \(timeElapsed) s.")
+    return results
+  }
+  
+      func printTimeElapsedWhenRunningCode(title:String, operation:()->()) {
+          let startTime = CFAbsoluteTimeGetCurrent()
+          operation()
+          let timeElapsed = CFAbsoluteTimeGetCurrent() - startTime
+          print("Time elapsed for \(title): \(timeElapsed) s.")
+      }
+}
